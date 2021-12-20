cbdf73484bfc4d3307135305c7fb1f5fc2d8333d
shreegowtham27 <gforgowtham8@gmail.com>
2021-12-20 19:51:48 +0530

    removed Styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from './Main';
 function App() {
   return (
     <div className="App">
-      <h1 style={{ color: "#F4F0E1", height: "100%", alignItems: "center", textAlign: "center", display: "flex", flexDirection: "column" }} >Test Page</h1>
+      <h1>Test Page</h1>
       <Main />
     </div>
   );
